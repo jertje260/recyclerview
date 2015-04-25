ad3c36e5fb7deb640fed5aecc29cd64fadceb4c9
jertje260 <jwbroekh@student.avans.nl>
2015-04-25 17:23:32 +0200

    initial

---
+++ app/src/main/java/com/example/du5fd/recyclerview/MyAdapter.java
@@ -0,0 +1,89 @@
+package com.example.du5fd.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Du5fd on 25-4-2015.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ItemViewHolder>  {
+
+    private ArrayList<String> mDataset;
+    private SparseBooleanArray selectedItems;
+
+    public MyAdapter(ArrayList<String> myDataset){
+        mDataset = myDataset;
+        selectedItems = new SparseBooleanArray();
+        this.print("something");
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        //final String name = mDataset.get(position);
+        holder.txtHeader.setText(mDataset.get(position));
+        holder.txtFooter.setText("Footer: " + mDataset.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public void print(String text){
+        Log.i("print", text);
+    }
+
+    public void toggleSelection(int pos){
+        if (selectedItems.get(pos, false)) {
+            selectedItems.delete(pos);
+        }
+        else {
+            selectedItems.put(pos, true);
+        }
+        notifyItemChanged(pos);
+    }
+
+    public void clearSelections(){
+        selectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount(){
+        return selectedItems.size();
+    }
+
+    public ArrayList<Integer> getSelectedItems() {
+        ArrayList<Integer> items =
+                new ArrayList<Integer>(selectedItems.size());
+        for (int i = 0; i < selectedItems.size(); i++) {
+            items.add(selectedItems.keyAt(i));
+        }
+        return items;
+    }
+
+    public final static class ItemViewHolder extends RecyclerView.ViewHolder {
+        public TextView txtHeader;
+        public TextView txtFooter;
+
+        public ItemViewHolder(View v) {
+            super(v);
+            txtHeader = (TextView) v.findViewById(R.id.firstLine);
+            txtFooter = (TextView) v.findViewById(R.id.secondLine);
+        }
+    }
+
+    public MyAdapter.ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        ItemViewHolder vh = new ItemViewHolder(v);
+        return vh;
+    }
+
+}
---
+++ app/src/main/java/com/example/du5fd/recyclerview/RecyclerListView.java
@@ -0,0 +1,159 @@
+package com.example.du5fd.recyclerview;
+
+import android.app.Activity;
+import android.support.v4.view.GestureDetectorCompat;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.ActionMode;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+public class RecyclerListView extends Activity implements RecyclerView.OnItemTouchListener, View.OnClickListener, ActionMode.Callback {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ArrayList<String> myDataset = new ArrayList<String>();
+    private ActionMode mActionMode;
+    private GestureDetectorCompat gestureDetector;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_list_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        fillDataset();
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.addOnItemTouchListener(this);
+        gestureDetector = new GestureDetectorCompat(this, new RecyclerListViewOnGestureListener());
+
+    }
+
+
+
+
+    private void fillDataset() {
+        myDataset.add("Hallo");
+        myDataset.add("Dit");
+        myDataset.add("is");
+        myDataset.add("een");
+        myDataset.add("test");
+        myDataset.add("wij");
+        myDataset.add("zijn");
+        myDataset.add("geselecteerd");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recycler_list_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        // Inflate a menu resource providing context menu items
+        MenuInflater inflater = mActionMode.getMenuInflater();
+        inflater.inflate(R.menu.menu_cab, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return false;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+       return false;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+        this.mActionMode = null;
+        mAdapter.clearSelections();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.list_item) {
+            // item click
+            int idx = mRecyclerView.getChildPosition(v);
+            if (mActionMode != null) {
+                myToggleSelection(idx);
+            }
+            //String data = mAdapter.getItem(idx);
+            // go to next view
+        }
+    }
+
+    private void myToggleSelection(int idx) {
+        mAdapter.toggleSelection(idx);
+        String title = getString(R.string.selected_count, mAdapter.getSelectedItemCount());
+        mActionMode.setTitle(title);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+        gestureDetector.onTouchEvent(motionEvent);
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView recyclerView, MotionEvent motionEvent) {
+
+    }
+
+    private class RecyclerListViewOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            View view = mRecyclerView.findChildViewUnder(e.getX(), e.getY());
+            onClick(view);
+            return super.onSingleTapConfirmed(e);
+        }
+
+        public void onLongPress(MotionEvent e) {
+            View view = mRecyclerView.findChildViewUnder(e.getX(), e.getY());
+            if (mActionMode != null) {
+                return;
+            }
+            // Start the CAB using the ActionMode.Callback defined above
+            mActionMode = startActionMode(RecyclerListView.this);
+            int idx = mRecyclerView.getChildPosition(view);
+            myToggleSelection(idx);
+            super.onLongPress(e);
+        }
+    }
+}
